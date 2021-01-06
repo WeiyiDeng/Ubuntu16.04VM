756574466827902d8ae454153b633564bbd1bd4a
daodaowei <weiyiwdeng@gmail.com>
2021-01-06 14:58:57 -0500

    feat: Summarize changes in around 50 characters or less
    More detailed explanatory text, if necessary. Wrap it to about 72
    characters or so. In some contexts, the first line is treated as the
    subject of the commit and the rest of the text as the body. The
    blank line separating the summary from the body is critical (unless
    you omit the body entirely); various tools like `log`, `shortlog`
    and `rebase` can get confused if you run the two together.
    
    Explain the problem that this commit is solving. Focus on why you
    are making this change as opposed to how (the code explains that).
    Are there side effects or other unintuitive consequences of this
    change? Here's the place to explain them.
    
    Further paragraphs come after blank lines.
    
     - Bullet points are okay, too
    
     - Typically a hyphen or asterisk is used for the bullet, preceded
       by a single space, with blank lines in between, but conventions
       vary here
    
    If you use an issue tracker, put references to them at the bottom,
    like this:
    
    Resolves: #123
    See also: #456, #789

--- TF/try_scale.py
+++ TF/try_scale.py
@@ -34,3 +34,7 @@ scaled = scale.transform([[2300, 1300]])
 predictedCO2 = regr.predict([scaled[0]])
 
 print('Predicted value is {} '.format(predictedCO2))
+
+# Scale: Center to the mean and component wise scale to unit variance.
+# CAUTION: Risk of data leak: Do not use scale unless you know what you are doing. A common mistake is to apply it to the entire data before splitting into training and test sets. This will bias the model evaluation because information would have leaked from the test set to the training set. In general, we recommend using StandardScaler within a Pipeline in order to prevent most risks of data leaking
+# https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.scale.html
